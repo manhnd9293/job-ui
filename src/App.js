532d436b1c0aa3424b75324193e07e2203fa773c
Manh Nguyen <manhnd9293@gmail.com>
2021-11-22 09:16:27 +0700

    create header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import { Home } from './view/home/Home';
-
+import classes from './app.module.css'
+import { Header } from './component/layout/header/Header';
+import { Footer } from './component/layout/footer/Footer';
 function App() {
   return (
-    <div className="App">
-      <Home></Home>
+    <div className={classes.app}>
+      <Header></Header>
+      <div className={classes.appBody}></div>
+      <Footer></Footer>
     </div>
   );
 }
