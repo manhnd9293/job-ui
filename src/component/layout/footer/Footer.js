0e09c30acb41ec51bfe4d6d0526a0090ca365b44
Manh Nguyen <manhnd9293@gmail.com>
2021-11-22 09:31:09 +0700

    add footer

--- src/component/layout/footer/Footer.js
+++ src/component/layout/footer/Footer.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import classes from './footer.module.css'
 export const Footer = () => {
     return (
-        <div>
-            
+        <div className={classes.container}>
+            <div style={{color: 'white'}}>Copyright by Manhnd - 2021</div>
         </div>
     )
 }
