bc14b1ddd50af6430082012d3fdabcb783857382
Manh Nguyen <manhnd9293@gmail.com>
2022-01-05 16:07:25 +0700

    search company feature

--- src/view/company/companySearch/ActionBar.js
+++ src/view/company/companySearch/ActionBar.js
@@ -1,50 +1,104 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import classes from "./companySearch.module.css";
-import { Link } from "react-router-dom";
+import {baseAxios} from "../../../config/AxiosConfig";
 
-export const ActionBar = () => {
-    return (
+export const ActionBar = ({onSearch}) => {
+    const [cityList, setCityList] = useState([]);
+    const [sizeList, setSizeList] = useState([]);
+    const [industryList, setIndustryList] = useState([]);
+    const [filterObject, setFilterObject] = useState({});
+
+    const industryLabel = 'choose industry';
+    const locationLabel = 'choose location';
+    const sizeLabel = 'choose company size';
+
+    useEffect(() => {
+        const industryPr = baseAxios.get('/company/list/industry');
+        const sizePr = baseAxios.get('/company/list/size');
+        const cityPr = baseAxios.get('/company/list/city');
+        Promise.all([industryPr, sizePr, cityPr]).then(values => {
+            const [industryRes, sizeRes, cityRes] = values;
+            setIndustryList(industryRes.data);
+            setSizeList(sizeRes.data);
+            setCityList(cityRes.data);
+        });
+    }, []);
+
+    function handleChangeIndustry(e) {
+        const value = e.target.value;
+        setFilterObject({...filterObject, ...{industry: value === industryLabel ? null : value}});
+    }
+
+    function handleChangeLocation(e) {
+        const value = e.target.value;
+        setFilterObject({...filterObject, ...{location: value === locationLabel ? null : value}});
+    }
+
+    function handleChangeSize(e) {
+        const value = e.target.value;
+        setFilterObject({...filterObject, ...{size: value === sizeLabel ? null : value}});
+    }
+
+
+    function handleChangeSearchKey(e) {
+        let value = e.target.value;
+        setFilterObject({...filterObject, ...{searchKey: (value ? value : null)}});
+    }
+
+  function handleSearchCompany() {
+    onSearch(filterObject);
+  }
+
+  return (
         <div className={classes.actionBar}>
-        <div>
-          <div>
-            <div>Search name</div>
-            <input style={{ margin: "10px 0px" }} type="text"></input>
-          </div>
-          <div>
-            <div>Industry</div>
-            <select
-              style={{ margin: "10px 0px" }}
-              placeholder="select industry"
-            >
-              <option>Information technology</option>
-            </select>
-          </div>
-          <div>
-            <div>Locaton</div>
-            <select
-              style={{ margin: "10px 0px" }}
-              placeholder="select industry"
-            >
-              <option>Hanoi</option>
-              <option>Danang</option>
-              <option>Hochiminh</option>
-            </select>
-          </div>
-          <div>
-            <div>Size</div>
-            <select
-              style={{ margin: "10px 0px" }}
-              placeholder="select industry"
-            >
-              <option>1-10 employees</option>
-              <option>10-100 employees</option>
-              <option>100-500 employees</option>
-            </select>
-          </div>
+            <div>
+                <div>
+                    <div>Search name</div>
+                    <input onChange={handleChangeSearchKey}
+                           style={{margin: "10px 0px"}} type="text"/>
+                </div>
+                <div>
+                    <div>Industry</div>
+                    <select
+                        style={{margin: "10px 0px"}}
+                        onChange={handleChangeIndustry}
+                    >
+                        <option key={`null`} value={null}>{industryLabel}</option>
+                        {industryList && industryList.map((industry, index) => (
+                            <option key={index} value={industry}>{industry}</option>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    <div>Locaton</div>
+                    <select
+                        style={{margin: "10px 0px"}}
+                        placeholder="select location"
+                        onChange={handleChangeLocation}
+                    >
+                        <option key={`null`} value={null}>{locationLabel}</option>
+                        {cityList && cityList.map((city, index) => (
+                            <option key={index} value={city}>{city}</option>
+                        ))}
+                    </select>
+                </div>
+                <div>
+                    <div>Size</div>
+                    <select
+                        style={{margin: "10px 0px"}}
+                        placeholder="select company size"
+                        onChange={handleChangeSize}
+                    >
+                        <option key={`null`} value={null}>{sizeLabel}</option>
+                        {sizeList && sizeList.map((size, index) => (
+                            <option key={index} value={size}>{`${size.trim().split('-').join(' ')} employees`}</option>
+                        ))}
+                    </select>
+                </div>
+            </div>
+            <div style={{textAlign: "center"}}>
+                <button onClick={handleSearchCompany}>Search company</button>
+            </div>
         </div>
-        <div style={{ textAlign: "center" }}>
-          <button>Search company</button>
-        </div>
-      </div>
     )
 }
--- src/view/company/companySearch/CompanySearch.js
+++ src/view/company/companySearch/CompanySearch.js
@@ -1,28 +1,43 @@
-import React, { useEffect, useState } from "react";
-import { ActionBar } from "./ActionBar";
+import React, {useEffect, useState} from "react";
+import {ActionBar} from "./ActionBar";
 import classes from "./companySearch.module.css";
-import { CompanyList } from "./CompanyList";
-import { baseAxios } from "../../../config/AxiosConfig";
-import {useLocation} from "react-router-dom";
+import {CompanyList} from "./CompanyList";
+import {baseAxios} from "../../../config/AxiosConfig";
+import {useLocation, useNavigate} from "react-router-dom";
+import {RoutePath} from "../../../constant/RouteConstant";
+
 export const CompanySearch = () => {
     const query = new URLSearchParams(useLocation().search);
-    const [companyList, setCompanyList] = useState([])
+    const [companyList, setCompanyList] = useState([]);
+    const [searching, setSearching] = useState(true);
+    let navigate = useNavigate();
+
     useEffect(() => {
         const searchKey = query.get('searchKey');
         const industry = query.get('industry');
-        const location = query.get('location');
+        const location = query.get('city');
         const size = query.get('size');
+        getCompanyList(searchKey,industry, location, size);
+        setSearching(false)
+    }, []);
 
-        baseAxios.get('/company').then((res) => {
+    const getCompanyList = (searchKey,industry,location,size) => {
+        baseAxios.get(`/company?searchKey=${searchKey || ''}&industry=${industry || ''}&city=${location || ''}&size=${size || ''}`).then((res) => {
             setCompanyList(res.data);
         })
-        
-    }, [])
-  
+    }
+
+    const searchCompany = (filterData) => {
+        const {searchKey, industry, location, size} = filterData;
+        getCompanyList(searchKey, industry, location, size);
+        navigate(`${RoutePath.ListCompany}?searchKey=${searchKey || ''}&industry=${industry || ''}&city=${location || ''}&size=${size || ''}`);
+
+    };
+
     return (
-    <div className={classes.container}>
-        <ActionBar></ActionBar>
-        <CompanyList companyList={companyList}></CompanyList>
-    </div>
-  );
+        <div className={classes.container}>
+            <ActionBar onSearch={searchCompany}/>
+            <CompanyList companyList={companyList}/>
+        </div>
+    );
 };
